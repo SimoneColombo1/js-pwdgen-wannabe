28d6379b4201f57d513b7fdc7fd34bcd9cd6084a
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-07 13:05:37 +0200

    create le variabili prompt per far inserire all'utente nome cognome e colore preferito/ scritto la mstringa per stamparla sul foglio html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ 
+const nome=prompt('inserire nome');
+const cognome=prompt('inserire cognome');
+const colore=prompt('inserire colore preferito');
+
+
+console.log(`${nome} ${cognome} ${colore} 24`)
+
+document.writeln(`${nome} ${cognome} ${colore} 24`)
